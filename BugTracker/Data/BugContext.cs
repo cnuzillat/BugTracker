a216f4debea05bb17f8c87bf1edd5eeadbdf8d58
cnuzillat <cnuzillat@gmail.com>
2025-11-25 22:24:56 -0500

    Created initial database configuration

--- BugTracker/Data/BugContext.cs
+++ BugTracker/Data/BugContext.cs
@@ -1,6 +1,15 @@
-﻿namespace BugTracker.Data
+﻿using BugTracker.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BugTracker.Data
 {
-    public class BugContext
+    public class BugContext : DbContext
     {
+        public BugContext(DbContextOptions<BugContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Bug> Bugs { get; set; }
     }
 }
--- BugTracker/Models/Bug.cs
+++ BugTracker/Models/Bug.cs
@@ -2,5 +2,11 @@
 {
     public class Bug
     {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Status { get; set; } = "Open"; // default
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
     }
 }
