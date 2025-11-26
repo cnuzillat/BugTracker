fc87cbd1c2b34fcaf65a8522366936a5c4e1545a
csn1296 <csn1296@g.rit.edu>
2025-11-25 22:09:25 -0500

    Updated file structure for prrojected project needs

--- BugTracker/Controllers/BugController.cs
+++ BugTracker/Controllers/BugController.cs
@@ -4,16 +4,16 @@ namespace BugTracker.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class WeatherForecastController : ControllerBase
+    public class BugController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
-        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly ILogger<BugController> _logger;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        public BugController(ILogger<BugController> logger)
         {
             _logger = logger;
         }
