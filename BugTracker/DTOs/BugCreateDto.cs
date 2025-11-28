c545c59499c96a81ae166afd531fc1c952753717
cnuzillat <cnuzillat@gmail.com>
2025-11-28 14:30:48 -0500

    Finished BugTrackerDto

--- BugTracker/DTOs/BugCreateDto.cs
+++ BugTracker/DTOs/BugCreateDto.cs
@@ -1,6 +1,17 @@
-﻿namespace BugTracker.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BugTracker.DTOs
 {
     public class BugCreateDto
     {
+        [Required]
+        [MaxLength(150)]
+        public string Title { get; set; } = null!;
+
+        [MaxLength(2000)]
+        public string? Description { get; set; }
+
+        [MaxLength(50)]
+        public string? Status { get; set; }
     }
 }
