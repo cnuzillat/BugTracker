362b5e59ff4853da5444e10ad98019568d41bf17
cnuzillat <cnuzillat@gmail.com>
2025-11-28 14:33:17 -0500

    Finished BugReadDto

--- BugTracker/DTOs/BugReadDto.cs
+++ BugTracker/DTOs/BugReadDto.cs
@@ -1,6 +1,17 @@
-﻿namespace BugTracker.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BugTracker.DTOs
 {
     public class BugReadDto
     {
+        public int Id { get; set; }
+
+        public string Title { get; set; } = null!;
+
+        public string? Description { get; set; }
+
+        public string? Status { get; set; }
+
+        public DateTime CreatedAt { get; set; }
     }
 }
