296f3cfea238b2ce2cc9d887a9a5a5c95bc7face
cnuzillat <cnuzillat@gmail.com>
2025-11-30 19:23:04 -0500

    Created BugService interface and completed BugService file

---
+++ BugTracker/Services/IBugService.cs
@@ -0,0 +1,13 @@
+﻿using BugTracker.DTOs;
+
+namespace BugTracker.Services
+{
+    public interface IBugService
+    {
+        Task<IEnumerable<DTOs.BugReadDto>> GetAllBugsAsync();
+        Task<DTOs.BugReadDto?> GetBugByIdAsync(int id);
+        Task<DTOs.BugReadDto> CreateBugAsync(BugCreateDto dto);
+        Task<DTOs.BugReadDto> UpdateBugAsync(int id, BugUpdateDto dto);
+        Task<bool> DeleteBugAsync(int id);
+    }
+}
